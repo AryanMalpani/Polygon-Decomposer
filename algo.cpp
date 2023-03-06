23b54ddc442330812508da26dd1af8e64ae01157
AryanMalpani <aryanmalpani02@gmail.com>
2023-03-07 01:16:25 +0530

    Add: Basic Algorithm before merging part

--- algo.cpp
+++ algo.cpp
@@ -2,3 +2,65 @@ using namespace std;
 
 #include <bits/stdc++.h>
 #include "dcel.h"
+
+bool isReflexAngle(Vertex A, Vertex B, Vertex C)
+{
+    //To complete
+    return true;
+}
+
+int main()
+{
+    DCEL p;
+    vector<DCEL> convex_polygons;
+
+    while(p.vertices.size()>3)
+    {
+        DCEL l;
+        Edge* p_start = &p.edges[0];
+        Edge* p_cursor = p_start;
+
+        l.addVertex(*p_cursor->org);
+        l.addVertex(*p_cursor->dest);
+
+        Edge* last_edge = l.addEdge(p_cursor->org, p_cursor->dest, NULL, NULL);
+
+        p_cursor = p_cursor->next;
+        Vertex next_vertex = *p_cursor->dest;
+
+        while(l.n<p.n && p_cursor != NULL &&
+            isReflexAngle(l.vertices[l.n-2], l.vertices[l.n-1], next_vertex) &&
+            isReflexAngle(l.vertices[l.n-1], next_vertex, l.vertices[0]) && 
+            isReflexAngle(next_vertex, l.vertices[0], l.vertices[1]))
+        {
+            l.addVertex(next_vertex);
+            last_edge = l.addEdge(p_cursor->org, p_cursor->dest, last_edge, NULL);
+            p_cursor = p_cursor->next;
+            next_vertex = *p_cursor->dest;
+        }
+
+        p_cursor = p_cursor->prev;
+        l.addEdge(&l.vertices[l.n-1], &l.vertices[0], p_cursor, p_start);
+
+        if(l.n<p.n)
+        {
+            vector<Vertex> interiors;
+            for(Vertex v:p.vertices)
+                if(l.findVertexIndex(v) == -1 && l.isInteriorPoint(v) == true)
+                    interiors.push_back(v);
+
+            while(!interiors.empty())
+            {
+                l.removeVertex(l.vertices[l.n-1]);
+                p_cursor = p_cursor->prev;
+                last_edge = l.addEdge(&l.vertices[l.n-1], &l.vertices[0], p_cursor, p_start);
+                for(int i=interiors.size()-1;i>=0;i--)
+                    if(!l.isInteriorPoint(interiors[i]))
+                        interiors.erase(interiors.begin() + i);
+            }
+            p.remove(l, p_cursor->next, p_start->prev);
+        }
+
+        convex_polygons.push_back(l);
+    }
+}
--- dcel.h
+++ dcel.h
@@ -2,9 +2,10 @@ using namespace std;
 
 #include <bits/stdc++.h>
 
+class Edge;
 
 /*
-    Class For Vertexes
+    Class For Vertices
 */
 class Vertex{
     public:
@@ -33,15 +34,15 @@ class Edge{
         Vertex *dest;
 
         //constructor
-        Edge(Edge *nextP, Edge *prevP, Edge *twinP, Vertex &origin, Vertex &destination);
+        Edge(Edge *nextP, Edge *prevP, Edge *twinP, Vertex *origin, Vertex *destination);
 };
 
-Edge::Edge(Edge *nextP, Edge *prevP, Edge *twinP, Vertex &origin, Vertex &destination) {
+Edge::Edge(Edge *nextP, Edge *prevP, Edge *twinP, Vertex *origin, Vertex *destination) {
     next = nextP;
     prev = prevP;
     twin = twinP;
-    org = &origin;
-    dest = &destination;
+    org = origin;
+    dest = destination;
 }
 
 
@@ -50,81 +51,83 @@ Edge::Edge(Edge *nextP, Edge *prevP, Edge *twinP, Vertex &origin, Vertex &destin
 */
 class DCEL {
     public:
-        set<Edge*> edges;
-        set<Vertex*> vertices;
-        int numberOfItems;
+        vector<Edge> edges;
+        vector<Vertex> vertices;
+        int n;
 
     public:
-
         DCEL();
-
-        void addEdge(Vertex &v1, Vertex &v2, Edge* e1prev, Edge* e1next);
-        void removeEdge(Edge &e1);
+        Edge* addEdge(Vertex* v1, Vertex* v2, Edge* e1prev, Edge* e1next);
+        void removeEdge(Edge* e1);
         void addVertex(Vertex &v);
         void removeVertex(Vertex &v);
         bool isInteriorPoint(Vertex &p);
+        void remove(DCEL &l, Edge* before_start, Edge* before_end);
+        Vertex* findVertex(Vertex &v);
+        int findVertexIndex(Vertex& v);
 };
 
 DCEL::DCEL() {
     edges.clear();
     vertices.clear();
-    numberOfItems=0;
+    n=0;
 }
 
 /*
 vertices v1 and v2 should be passed in clockwise order
 e1prev is the previous of the edge made from v1 to v2
 e1next is the next
-both e1prev and e1next are pointers use & operator for passing normal Edge datatype 
+both e1prev and e1next are pointers
 pass NULL values if prev or next not known
 */
-void DCEL::addEdge(Vertex& v1, Vertex& v2, Edge* e1prev, Edge* e1next)
+Edge* DCEL::addEdge(Vertex* v1, Vertex* v2, Edge* e1prev, Edge* e1next)
 {
     //insert vertices if not already present
-    vertices.insert(&v1);
-    vertices.insert(&v2);
+    addVertex(*v1);
+    addVertex(*v2);
 
     //check if edges already exist
-    for(auto e:edges)
-        if(e->org == &v1 && e->dest == &v2)
-            return ;
+    for(int i=0;i<edges.size();i++)
+        if(edges[i].org == v1 && edges[i].dest == v2)
+            return &edges[i];
 
-    Edge* e1 = &Edge(NULL, NULL, NULL, v1, v2);
-    Edge* e2 = &Edge(NULL, NULL, NULL, v2, v1);
+    Edge e1 = Edge(NULL, NULL, NULL, v1, v2);
+    Edge e2 = Edge(NULL, NULL, NULL, v2, v1);
 
-    e1->twin = e2;
-    e2->twin = e1;
+    e1.twin = &e2;
+    e2.twin = &e1;
 
-    v1.inc_edges.insert(e1);
-    v2.inc_edges.insert(e2);
+    v1->inc_edges.insert(&e1);
+    v2->inc_edges.insert(&e2);
 
     if(e1prev != NULL)
     {
-        e1->prev = e1prev;
-        e2->next = e1prev->twin;
-        e1prev->next = e1;
-        e1prev->twin->prev = e2;
+        e1.prev = e1prev;
+        e2.next = e1prev->twin;
+        e1prev->next = &e1;
+        e1prev->twin->prev = &e2;
     }
 
     if(e1next != NULL)
     {
-        e1->next = e1next;
-        e2->prev = e1next->twin;
-        e1next->prev = e1;
-        e1next->twin->next = e2;
+        e1.next = e1next;
+        e2.prev = e1next->twin;
+        e1next->prev = &e1;
+        e1next->twin->next = &e2;
     }
 
-    edges.erase(e1);
-    edges.erase(e2);
+    edges.push_back(e1);
+    edges.push_back(e2);
+
+    return &edges[edges.size()-2];
 }
 
 /*
-will remove the edge e as well as its twin
+will remove the edge e1 as well as its twin
 */
-void DCEL::removeEdge(Edge &e)
+void DCEL::removeEdge(Edge* e1)
 {
-    Edge* e1 = &e;
-    Edge* e2 = e1->twin;
+    Edge *e2 = e1->twin;
 
     e1->org->inc_edges.erase(e1);
     e2->org->inc_edges.erase(e2);
@@ -141,11 +144,10 @@ void DCEL::removeEdge(Edge &e)
         e1->next->prev = NULL;
     }
 
-    edges.erase(e1);
-    edges.erase(e2);
-
-    free(e1);
-    free(e2);
+    for(int i=edges.size()-1;i>=0;i++)
+        if((edges[i].org==e1->org && edges[i].org==e1->dest)||
+            (edges[i].org==e2->org && edges[i].org==e2->dest))
+            edges.erase(edges.begin()+i);
 }
 
 /*
@@ -153,7 +155,12 @@ will add vertex v to the DCEL
 */
 void DCEL::addVertex(Vertex &v)
 {
-    vertices.insert(&v);
+    int i = findVertexIndex(v);
+    if(i==-1)
+        return ;
+
+    vertices.push_back(v);
+    n++;
 }
 
 /*
@@ -162,11 +169,15 @@ will remove the vertex v as well as all the edges associated with it
 void DCEL::removeVertex(Vertex &v)
 {
     for(auto e:v.inc_edges)
-        removeEdge(*e);
+        removeEdge(e);
 
-    vertices.erase(&v);
+    int i = findVertexIndex(v);
+    if(i==-1)
+        return ;
 
-    free(&v);
+    vertices.erase(vertices.begin() + i);
+
+    n--;
 }
 
 /*
@@ -176,9 +187,37 @@ bool DCEL::isInteriorPoint(Vertex &p)
 {
     int count = 0;
     for(auto e:edges)
-        if((p.y < e->org->y != p.y < e->dest->y) && 
-        (p.x < (e->dest->x-e->org->x) * (p.y-e->org->y) / (e->dest->y-e->org->y) + e->org->x))
+        if((p.y < e.org->y != p.y < e.dest->y) && 
+        (p.x < (e.dest->x-e.org->x) * (p.y-e.org->y) / (e.dest->y-e.org->y) + e.org->x))
             count++;
 
     return count%2 == 1;
+}
+
+void DCEL::remove(DCEL &l, Edge* before_start, Edge* after_end)
+{
+    addEdge(after_end->org, before_start->dest, after_end->prev, before_start->next);
+
+    for(Vertex v:l.vertices)
+        removeVertex(v);
+}
+
+Vertex* DCEL::findVertex(Vertex& v)
+{
+    int i;
+    for(i=0;i<vertices.size();i++)
+        if(vertices[i].x==v.x && vertices[i].y==v.y)
+            break;
+    
+    return i==vertices.size()?NULL:&vertices[i];    
+}
+
+int DCEL::findVertexIndex(Vertex& v)
+{
+    int i;
+    for(i=0;i<vertices.size();i++)
+        if(vertices[i].x==v.x && vertices[i].y==v.y)
+            break;
+    
+    return i==vertices.size()?-1:i;    
 }
